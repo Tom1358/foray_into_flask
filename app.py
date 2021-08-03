5d45a5704260ab67b13439cfbd1ab4bec449c990
Tom1358 <thomasellis358@hotmail.com>
2021-08-03 18:40:23 +0000

    Add html files base, index, about, contact, and careers, and use Template Inheritence to format the HTML code in them

--- app.py
+++ app.py
@@ -2,11 +2,27 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__) # creating instance of Flask, & storing in variable 'app'
-    
+
 
 @app.route("/") # a decorator (pie-notation) - '/' browses root directory
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
